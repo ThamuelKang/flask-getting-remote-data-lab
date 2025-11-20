50bcc449ba4e879fb69da6bdd3b224508a1fa76a
Samuel <duckbutt@Samuels-Mac-Studio.local>
2025-11-19 16:05:35 -0800

    update

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,17 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self): 
+        self.response = requests.get(self.url)
+        return self.response.content
 
     def load_json(self):
-        pass
+        self.get_response_body()
+        formatted = self.response.json()
+        print(formatted)
+        return formatted
+
+requester = GetRequester("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+
+requester.get_response_body()
+requester.load_json()
